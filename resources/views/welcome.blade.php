8554f9b646fa8e7e3a0a6d9eecb530120feb234b
abartolomego18dw <abartolomego18dw@ikzubirimanteo.com>
2019-11-08 13:34:50 +0100

    Probando unión de github con slack

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    No Laravel
                 </div>
 
                 <div class="links">
