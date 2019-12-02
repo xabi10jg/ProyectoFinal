797dd1193c8ccab5d6bd0a83f304f72262857dc1
xabi10jg <xjacobgo18dw@ikzubirimanteo.com>
2019-12-02 13:05:42 +0100

    middleware lang

---
+++ app/Http/Middleware/SetLang.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Session;
+use App;
+use Config;
+
+class SetLang
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Session::has("locale")){
+           
+        \App::setlocale(\Session::get('locale'));
+
+        }   
+        return $next($request);
+    
+    }
+}
