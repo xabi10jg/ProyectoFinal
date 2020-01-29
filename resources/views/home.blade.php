f0498bfc30bfe127cc31893932ef4727e38ed9dd
Jonva12 <jonva12@hotmail.com>
2020-01-29 14:02:45 +0100

    correccion estilo home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,24 +16,26 @@
                     <div class="col-lg-12 text-center">
                         <h2 class="section-heading text-uppercase">@lang('Perfil Usuario')</h2>
                     </div>
-                    <div class="text-black">
+                    <div class="text-black justify-content-center">
                         <div class="row col-lg-12 mx-auto">
-                            <div class="col-lg-4">@lang('Nombre'): {{Auth()->user()->name}}</div>
-                            <div class="col-lg-4">@lang('Apellido'): {{Auth()->user()->apellido}}</div>
-                            <div class="col-lg-4">E-mail: {{Auth()->user()->email}}</div>
+                            <div class="col-lg-6">@lang('Nombre'): {{Auth()->user()->name}}</div>
+                            <div class="col-lg-6">@lang('Apellido'): {{Auth()->user()->apellido}}</div>
                         </div><br>
                         <div class="row col-lg-12 mx-auto">
-                            <div class="col-lg-4">@lang('Direccion'): {{Auth()->user()->direccion}}</div>
-                            <div class="col-lg-4">@lang('Localidad'): {{Auth()->user()->localidad}}</div>
-                            <div class="col-lg-4">@lang('Provincia'): {{Auth()->user()->provincia}}</div>
+                            <div class="col-lg-6">E-mail: {{Auth()->user()->email}}</div>
+                            <div class="col-lg-6">@lang('Direccion'): {{Auth()->user()->direccion}}</div>
                         </div><br>
                         <div class="row col-lg-12 mx-auto">
-                            <div class="col-lg-4">@lang('Pais'): {{Auth()->user()->pais}}</div>
-                            <div class="col-lg-4">@lang('Telefono'): {{Auth()->user()->telefono}}</div>
-                            <div class="col-lg-4">@lang('Rol'): {{Auth()->user()->roles->rol}}</div>
+                            <div class="col-lg-6">@lang('Localidad'): {{Auth()->user()->localidad}}</div>
+                            <div class="col-lg-6">@lang('Provincia'): {{Auth()->user()->provincia}}</div>
                         </div><br>
                         <div class="row col-lg-12 mx-auto">
-                            <div class="col-lg-12">Te uniste en {{Auth()->user()->email_verified_at}}</div>
+                            <div class="col-lg-6">@lang('Pais'): {{Auth()->user()->pais}}</div>
+                            <div class="col-lg-6">@lang('Telefono'): {{Auth()->user()->telefono}}</div>
+                        </div><br>
+                        <div class="row col-lg-12 mx-auto">
+                            <div class="col-lg-6">@lang('Rol'): {{Auth()->user()->roles->rol}}</div>
+                            <div class="col-lg-6">Te uniste en {{Auth()->user()->email_verified_at}}</div>
                         </div><br>
                         <div class="row col-lg-12 mx-auto">
                             <div class="col"><a class="btn btn-primary" href="{{route('FormularioEditar', Auth::user()->id)}}">Editar</a></div>
