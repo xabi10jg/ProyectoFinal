89c09b49fcfe708ba6f55037628e1f8cccb42222
Jonva12 <jonva12@hotmail.com>
2020-01-31 10:52:07 +0100

    api terminada y chart de usuarios registrados cada mes

---
+++ app/Http/Controllers/apiController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mascota;
+use App\User;
+use App\Organizacion;
+use App\Tipo;
+use App\Role;
+
+class apiController extends Controller
+{
+    public function mascotas()
+    {
+        $mascotas = Mascota::all();
+        return $mascotas;
+    }
+
+    public function usuarios(){
+        $usuarios = User::all();
+        return $usuarios;
+    }
+
+    public function organizaciones(){
+        $orgs = Organizacion::all();
+        return $orgs;
+    }
+    //devuelve el listado de usuarios registrados ese año y suma los de cada mes
+    public function userYear($year){
+        $usuariosMes=array();
+        if(isset($year)){
+            for($i = 1; $i < 13; $i++){
+                $usuarios = User::whereYear('created_at', $year)->whereMonth('created_at', $i)->count();
+                //dd($usuarios);
+                array_push($usuariosMes, ['mes'=>$i, 'usuarios'=>$usuarios]);
+            }
+        }else{
+            $year = 2020;
+            for($i = 1; $i < 13; $i++){
+                $usuarios = User::where('created_at', $year)->whereMonth('created_at', $i)->count();
+                array_push($usuariosMes, ['mes'=>$i, 'usuarios'=>$usuarios]);
+            }
+        }
+        
+        return $usuariosMes;
+    }
+}
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,6 +16,8 @@
   <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
   <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
   <!-- Custom styles for this template-->
   <link href="{{asset('/zonaAdmin/css/sb-admin-2.min.css')}}" rel="stylesheet">
