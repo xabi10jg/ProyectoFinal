b1268d3f11323232f0c1aa6a8a293301c87cc0d0
Jonva12 <jonva12@hotmail.com>
2020-02-06 23:00:01 +0100

    cambios editar organizacion

--- resources/views/admin/editOrg.blade.php
+++ resources/views/admin/editOrg.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <div class="col-lg-10 text-center text-gray-900">
   <h1>Editar Organizacion</h1>
-  <form method="post" class="text-center" action="{{route('org.store')}}">
+  <form method="post" class="text-center" action="{{route('org.update', $org->id)}}">
+    @method('PUT')
     @csrf
     <div class="form-row">
       <div class="col-lg-6">
